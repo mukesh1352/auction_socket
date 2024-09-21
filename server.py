4db9150162cb1d907ca69626793d49428a208bca
mukesh <mukeshnarravula14@gmail.com>
2024-09-21 20:22:56 +0530

    Started the coding part

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import socket
 import mysql.connector
-from twisted.conch.insults.window import cursor
+import threading
+import time
 
-
+initial_purse_amount = 10000
+extra_purse_amount = 10000
 def database():
     try:
         conn = mysql.connector.connect(
@@ -12,10 +14,25 @@ def database():
             database = "player_info"
         )
         cursor = conn.cursor()
+        return conn,cursor
     except mysql.connector.Error as err:
         print(f"Database error:{err}")
         exit(1)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-s.bind(('localhost',8888 ))
+s.bind(('localhost',8888 )) #binding with the host
+
+s.listen(5) #listen to 5 clients
+
+def handle_client(c,addr):
+    print(f"Connected with the client at {addr}")
+    print(f"Welcome to the IPL AUCTION\n".encode())
+
+def accept_connection():
+    while True:
+        c,addr = s.accept()
+        client_thread = threading.Thread(target=handle_client,args=(c,addr))
+        client_thread.start()
+
+accept_connection()
