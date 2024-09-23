cf010486adc500665e4b5811ad5a8c43b45d688c
mukesh <mukeshnarravula14@gmail.com>
2024-09-23 21:57:59 +0530

    Successfully connected to the database

--- server.py
+++ server.py
@@ -1,5 +1,29 @@
 import socket
+import mysql.connector
+from mysql.connector import Error
 
+def database():
+    """Establish a connection to the MySQL database."""
+    try:
+        # Define the connection parameters
+        connection = mysql.connector.connect(
+                host='localhost',      
+                user='mukesh',            
+                password='V3nasaur@123',  
+                database='players'
+        )
+
+        if connection.is_connected():
+            print("Successfully connected to the database.")
+            return connection
+
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+        return None
+
+    return None
+
+# Create socket server
 clients = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -7,12 +31,15 @@ s.bind(('localhost', 8888))
 s.listen(3)
 print("Waiting for connections.\n")
 
+# Connect to the database
+db_connection = database()
+
 while True:
     try:
         c, addr = s.accept()
         print(f"Connected with the client at addr: {addr}")
         c.sendall(b"Welcome to the IPL AUCTION.\n")
-        
+
         # Send options after connection
         options = (
             "Choose one of the below options:\n"
